4e2eb1917863155f4a62de5af756feed0b1a889c
PiotrMokwa <mokwapiotr@gmail.com>
2024-08-21 21:31:35 +0200

    Lesson4 (#5)
    * Lesson 4 - version 1
    
    * Lesson 4 - version 2
    
    * Lesson 4 - version 3
    
    * Lesson 4 - version 4
    
    * Lesson 4 - version 5

--- app/src/main/java/pl/futurecollars/invoicing/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/App.java
@@ -1,9 +1,12 @@
 package pl.futurecollars.invoicing;
 
+import org.springframework.boot.SpringApplication;
+import pl.futurecollars.invoicing.service.InvoiceController;
+
 public class App {
 
   public static void main(String[] args) {
-  // SpringApplication.run(InvoiceController.class, args); Uruchomienie springa
+    SpringApplication.run(InvoiceController.class, args);
   }
 }
 
