523d118d5db13e90d6c70c5d2ff3556a562c3e3c
Piotr.Mokwa <mokwapiotr@gmail.com>
2025-01-09 16:39:13 +0100

    Lesson 17 - Angular check style correction

--- app/src/main/java/pl/futurecollars/invoicing/controller/company/CompaniesControllerApi.java
+++ app/src/main/java/pl/futurecollars/invoicing/controller/company/CompaniesControllerApi.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.futurecollars.invoicing.model.Company;
 
-
 @RequestMapping(value = "companies", produces = {"application/json;charset=UTF-8"})
 @Api(tags = {"companies controller"})
 public interface CompaniesControllerApi {
